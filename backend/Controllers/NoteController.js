23ff40d2dbfc5b442629fbe3bdd77d86ad9a2295
devlucasalmd <lucas.amaral.almd@gmail.com>
2023-10-09 11:33:05 -0300

    Backend

--- backend/Controllers/NoteController.js
+++ backend/Controllers/NoteController.js
@@ -12,12 +12,39 @@ module.exports = class NotesController{
         }
     }
     static async createNote(req,res){
-        res.status(200).json ({message: "rota para criar um"})
+        // res.status(200).json ({message: "rota para criar um note"})
+        const {title, description} = req.body;
+        if(!title){
+            return res.status(402).json({message: "O title é obrigatório"})            
+        }
+        if(!description){
+            return res.status(402).json({message: "O description é obrigatório"})            
+        }
+        try{
+            const note = await Note.create({title, description})
+            res.status(201).json({message: "criado com sucesso", note: note})
+        } catch(error) {
+            console.log(error)
+            res.status(500).json({error: "algo deu errado"})
+        }
     }
     static async deleteNote(req,res){
-        res.status(200).json ({message: "rota para deletar um"})
-    }
+        //res.status(200).json ({message: "rota para deletar um note"})
+        const {id} =req.params;
+        try {
+        const note = await Note.findByPk(id);
+        if(note){
+            await note.destroy();
+            res.status(201).json({message: "anotação deletada com sucesso"})
+        }else{
+            res.status(404).json({message: "anotação não encontrada"})
+        }
+        } catch (error) {
+        console.log(error)
+        res.status(500).json({error: "erro ao tentar deletar a chave"})
+        }
+        }
     static async editNote(req,res){
-        res.status(200).json ({message: "rota para editar um"})
+        res.status(200).json ({message: "rota para editar um note"})
     }
 }
